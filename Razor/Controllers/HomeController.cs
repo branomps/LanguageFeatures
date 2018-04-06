0bd3d5fad4adecd7af7558b3cb69992dc9603edf
Branislav Macosinec <branislav.macosinec@windowslive.com>
2018-04-06 17:21:28 +0100

    Use of Array and enumerating an Array in a Razor. Index

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -9,16 +9,16 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
             ViewBag.StockLevel = 5;
-            Product myProduct = new Product()
+            Product[] myProduct =
             {
-                ProductID = 1,
-                Name = "Kayak",
-                Description = "A boaat for one person",
-                Category = "Watersports",
-                Price = 275M
+                new Product {Name ="Kayak", Price = 275M},
+                new Product {Name ="Lifejacket", Price = 48.50M},
+                new Product {Name ="Soccer ball", Price = 19.50M},
+                new Product {Name ="Corner flag" , Price = 34.95M},
+                new Product {Name ="Soccer Web", Price = 20M}
             };
             return View(myProduct);
         }
