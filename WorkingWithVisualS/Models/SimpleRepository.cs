602951103c86510e794aaf2651126d067b24bab7
Branislav Macosinec <branislav.macosinec@windowslive.com>
2018-04-06 17:42:27 +0100

    Add a Simple Repository with using of dictionary.

---
+++ WorkingWithVisualS/Models/SimpleRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithVisualS.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> product = new Dictionary<string, Product>();
+
+        public static SimpleRepository SharedRepository => sharedRepository;
+
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product {Name ="Kayak", Price = 275M},
+                new Product {Name ="Lifejacket", Price = 48.50M},
+                new Product {Name ="Soccer ball", Price = 19.50M},
+                new Product {Name ="Corner flag" , Price = 34.95M},
+                new Product {Name ="Soccer Web", Price = 20M}
+            }
+        }
+    }
+}
