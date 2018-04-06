d8d5d632c2fb087273426b30a7167b585b3f9e19
Branislav Macosinec <branislav.macosinec@windowslive.com>
2018-04-06 19:46:21 +0100

    Bower packages

--- WorkingWithVisualS/Models/SimpleRepository.cs
+++ WorkingWithVisualS/Models/SimpleRepository.cs
@@ -21,7 +21,7 @@ namespace WorkingWithVisualS.Models
                 new Product {Name ="Soccer ball", Price = 19.50M},
                 new Product {Name ="Corner flag" , Price = 34.95M},
                 new Product {Name ="Soccer Web", Price = 20M}
-            }
+            };
         }
     }
 }
